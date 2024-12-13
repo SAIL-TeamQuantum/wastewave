ed5f6d1af437adc64a66f93fb172b42ddd90a7e3
lanre2222 <olanrewajulanre92@gmail.com>
2024-12-13 09:56:06 +0100

    first commit

---
+++ src/Reusables/Buttonprops.jsx
@@ -0,0 +1,103 @@
+import React from "react";
+import styled from "styled-components";
+
+const ButtonProps = ({ h3 }) => (
+    <Buttondiv>
+      <StyledHeading>{h3 || "Please Select an Option"}</StyledHeading>
+      <ResidentButton>Resident</ResidentButton>
+      <CompanyButton>Waste Management Company</CompanyButton>
+      <AgencyButton>Waste Management Agency</AgencyButton>
+    </Buttondiv>
+  );
+  
+const Buttondiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 15px;
+  background-color: white;
+
+
+`;
+
+const StyledHeading = styled.h3`
+
+font-size: 16px;
+font-weight: 400;
+line-height: 21.6px;
+text-align: left;
+/* margin-bottom: 20px; */
+color: #A1A4B2;
+
+
+`;
+
+const ResidentButton = styled.button`
+    background-color: #81B622;
+    color: white;
+    width: 374px;
+    height: 63px;
+    gap: 0px;
+    border-radius: 38px ;
+    opacity: 0px;
+    border: none;
+    cursor: pointer;
+    font-size: 20px;
+    font-weight: 700;
+    line-height: 21.62px;
+    letter-spacing: 0.05em;
+    text-align: center;
+    
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
+
+const CompanyButton = styled.button`
+  background-color: white;
+  color: #81B622;
+   width: 374px;
+    height: 63px;
+    gap: 0px;
+    border-radius: 38px ;
+    opacity: 0px;
+    border: 3px solid #81B622;
+    cursor: pointer;
+    font-family: Montserrat;
+    font-size: 20px;
+    font-weight: 500;
+    line-height: 21.62px;
+    letter-spacing: 0.05em;
+    text-align: center;
+
+  &:hover {
+    background-color: #1e7e34;
+  }
+`;
+
+const AgencyButton = styled.button`
+    background-color: white;
+    color: #81B622;
+    width: 374px;
+    height: 63px;
+    gap: 0px;
+    border-radius: 38px ;
+    opacity: 0px;
+     border: 3px solid #81B622;
+    cursor: pointer;
+    font-size: 20px;
+    font-weight: 500;
+    line-height: 21.62px;
+    letter-spacing: 0.05em;
+    text-align: center;
+
+
+  &:hover {
+    background-color: #e0a800;
+  }
+`;
+
+
+export default ButtonProps;
+
---
+++ src/components/UserselectMenu.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import NEWWASTE from '../assets/logo.png'
+import styled from "styled-components";
+import ButtonProps from "../Reusables/Buttonprops";
+
+
+
+
+const UserSelectMenu = () => {
+    return (
+    <MenuDiv>
+        <img src={NEWWASTE} alt=""/>
+        <h2>Which option best describes <br /> your position in the waste  <br />management 
+        chain?</h2>
+        <ButtonProps/>
+     
+      
+      
+    </MenuDiv>);
+}
+ 
+export default UserSelectMenu;
+
+const MenuDiv = styled.div`
+
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+margin-top: 100px;
+
+h2{
+    font-weight: 500;
+    font-size: 24px;
+    line-height: 39.6px;
+    color: #004AAD;
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+`
+
+export {MenuDiv}
