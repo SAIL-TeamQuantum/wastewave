0136a9df8f0a0dc0ced42cade9359e39aab226e5
lanre2222 <olanrewajulanre92@gmail.com>
2025-01-08 14:45:30 +0100

    resolved astute conflict

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,40 @@ import './App.css'
 import Profile from './pages/Home'
 import AboutDMX from './pages/AboutDMX'
 import AddressDMX from './pages/AddressDMX'
-import HomeDMX from './pages/HomeDMX'
+import HomeDMX from './components/Home'
+import DashboardAST from './pages/DashboardAST';
+import ScheduleAST from './pages/ScheduleAST' ;
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-function App() {
-  const [count, setCount] = useState(0)
+import React from 'react'
+import UserSelectMenu from './components/UserselectMenu';
+import LoginPage from './components/LoginPage';
+import SignUpProps from './Reusables/SignupProps';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Landing from './components/Landing';
+import LandingPage from './pages/LandingPageDAMI'
 
-  return (
-    <>
-      {/* <NavBarDMX /> */}
-      <BrowserRouter>
-                <Routes>
-                    <Route path='/' element={<HomeDMX />}/>
-                    <Route path='/profile' element={<Profile />}/>
-                    <Route path='/about' element={<AboutDMX/>}/>
-                    <Route path='/address' element={<AddressDMX/>}/>
-                </Routes>
-            
-      </BrowserRouter>
-    </>
-  )
-}
+const app = () => {
+  return ( 
+  <div>
+    <Router>
+    <Routes>
+      <Route path='/' element={ <Landing/>}/> 
+      <Route path='/UserSelectMenu' element={ <UserSelectMenu/>}/>
+      <Route path='/SignUpProps' element={ <SignUpProps/>}/>
+      <Route path='LoginPage' element={ <LoginPage/>}/>
+      <Route path='/' element={<HomeDMX />}/>
+      <Route path='/profile' element={<Profile />}/>
+      <Route path='/about' element={<AboutDMX/>}/>
+      <Route path='/address' element={<AddressDMX/>}/>
+      <Route path='/' element={<LandingPage/>}/>
+      <Route path='/DashboardAST' element={<DashboardAST />}/>
+      <Route path='/ScheduleAST' element={<ScheduleAST />}/>
 
-export default App
+    </Routes>
+    </Router>
+    </div> 
+   );
+  }
+
+
+export default app;
