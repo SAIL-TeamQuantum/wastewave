85e130274da2fc9e46b9b021e8fe38a44925b78e
lanre2222 <olanrewajulanre92@gmail.com>
2025-01-14 11:45:29 +0100

    made some changes

--- src/components/UserselectMenu.jsx
+++ src/components/UserselectMenu.jsx
@@ -7,8 +7,8 @@ import Calendar from '../assets/Calendar.png';
 import HandHear from '../assets/HandHear.png';
 import HandHeart from '../assets/HandHeart.png';
 import Radioactive from '../assets/Radioactive.png';
-// import Shieldplus from '../assets/Shieldplus.png';
-import Shieldplu from '../assets/Shieldplu.png';
+// import Shieldplus from '../assets/ShieldPlus.png';
+import Shieldplu from '../assets/ShieldPlu.png';
 import NavHeader from "../components/Header";
 
 
